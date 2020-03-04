cb37a250b60570aa9975fa16236289209521ba95
khatabwedaa <khatabwedaa@gmail.com>
2020-03-04 17:47:10 +0200

    build a game & design it

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="bg-gray-800 text-gray-100" id="app">
-    Hello
+  <div class="flex items-center justify-center h-screen px-8 py-4" id="app">
+    <div class="md:flex items-center container">
+      <div class="w-full md:w-1/2">
+        <img class="w-4/5" src="../src/assets/img/undraw_dev_productivity_umsq.svg" alt="man typing...">
+      </div>
+      <div class="w-full text-center text-gray-800 md:w-1/2">
+        <type-component></type-component>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+  import TypeComponent from "./components/TypeComponent";
+  export default {
+    components: { TypeComponent }
+  }
 </script>
 
 <style>
---
+++ src/components/TypeComponent.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-semibold">How Fast You Are ?</h1>
+        <div class="text-2xl mt-6">Type this word : <span class="text-blue-600 font-semibold">{{ word }}</span></div>
+        <div class="flex justify-between">
+            <div class="text-2xl">Time : <span class="text-blue-600 font-semibold">{{ countDown }}</span></div>
+            <div class="text-2xl">Score : <span class="text-blue-600 font-semibold">{{ countScore }}</span></div>
+        </div>
+        <input v-model="model" class="mt-6 bg-gray-300 text-gray-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 block w-full appearance-none" type="email">
+        <button class="w-full mt-4 px-4 py-2 bg-blue-600 text-white uppercase rounded" @click="restart" v-if="countDown == 0">restart</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                word: 'start',
+                countDown: 5,
+                model: '',
+                words: ['php' , 'vue' , 'bostman' , 'api' , 'laravel' , 'laracon' , 'laracast'],
+                countScore: 0 ,
+                arr: 0
+            }
+        },
+        methods : {
+            countDownTimer() {
+                if(this.countDown > 0) {
+                    setTimeout(() => {
+                        this.countDown -= 1
+                        console.log(this.isTrue())
+                        this.countDownTimer()
+                    }, 1000)
+                }
+            },
+            isTrue() {
+                if(this.word == this.model)
+                {
+                    this.countDown = 5
+                    this.model = ''
+                    this.word = this.words[this.arr]; this.arr++
+                    this.countScore += 5
+                    return "Yes"
+                }
+
+                return "NO"
+            },
+            restart() {
+                this.model = ""
+                this.arr = 0
+                this.word = this.words[this.arr]; this.arr++
+                this.countScore = 0
+                this.countDown = 5
+                this.countDownTimer()
+            }
+        },
+        mounted() {
+            this.countDownTimer()
+        },
+    }
+</script>
